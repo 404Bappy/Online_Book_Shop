e338b0905a8a21427fd41b73eda6668e6c7c232f
Bappy71 <19103076@iubat.edu>
2022-12-07 04:18:48 +0600

    first commit

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ window.onscroll = () =>{
     }
 }
 
-window.onscroll = () =>{
+window.onload = () =>{
     
     if(window.scrollY > 80){
         document.querySelector('.header .header-2').classList.add('active');
