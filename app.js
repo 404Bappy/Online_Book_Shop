bef4121f7f04e3c6e83fa32788093b9958f4dd56
Bappy71 <19103076@iubat.edu>
2022-12-06 23:49:53 +0600

    first commit

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 window.onscroll = () =>{
+    
     if(window.scrollY > 80){
-        document.querySelector('.header .header-2').classList.add
+        document.querySelector('.header .header-2').classList.add('active');
+    }else{
+        document.querySelector('.header .header-2').classList.add('active');
+    }
+}
+
+window.onscroll = () =>{
+    
+    if(window.scrollY > 80){
+        document.querySelector('.header .header-2').classList.add('active');
+    }else{
+        document.querySelector('.header .header-2').classList.add('active');
     }
 }
