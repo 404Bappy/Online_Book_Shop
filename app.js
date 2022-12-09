4cf7bcaa9229b6dcdd9d5767ef528747fffd0d1d
Bappy71 <19103076@iubat.edu>
2022-12-10 01:03:44 +0600

    first commit

--- app.js
+++ app.js
@@ -64,6 +64,12 @@ var swiper = new Swiper(".featured-slider", {
         delay: 9500,
         disableOnInteraction: false,
     },
+
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+
     breakpoints: {
         0: {
             slidesPerView: 1,
